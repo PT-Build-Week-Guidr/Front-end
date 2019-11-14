2d4c9d877c4f7e79bd01859cc7e17d68d47d7194
Trevor Thompson <trevthom18@gmail.com>
2019-11-13 20:35:23 -0500

    Successfully added two forms to my WelcomePage, and begun implementing skeletlon event listener code

--- guidr/src/components/WelcomePage.js
+++ guidr/src/components/WelcomePage.js
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 function WelcomePage () {
 
+    const [username, setUserName] = useState([]);
+    const [password, setPassword] = useState([]);
+
+    const handleUserName = event => {
+
+        setUserName(username);
+
+    }
+
+    const handlePassword = event => {
+
+        setPassword(password);
+
+    }
+
     return (
-        <header>
+        <section className="login-form">
             <h1>Welcome to Guidr!</h1>
-        </header>
+            <form>
+               <label>
+                   Username:
+                <input
+                type="text"
+                placeholder="UserName"
+                username="username"
+                onChange={handleUserName}
+                />
+               </label> 
+               <br></br>
+               <br></br>
+               <label>
+                   Password:
+                   <input
+                   type="text"
+                   password="password"
+                   placeholder="Password"
+                   onChange={handlePassword}
+                   />
+               </label>
+            </form>
+        </section>
         
     );
     
