0f83d3f6e59fb7c8ebda6c110bd95f046bc2143e
Trevor Thompson <trevthom18@gmail.com>
2019-11-13 19:51:41 -0500

    Added components directory, and begun working on WelcomePage component

---
+++ guidr/components/WelcomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function WelcomePage () {
+
+    return (
+        <header>
+            <h1>Welcome to Guidr!</h1>
+        </header>
+        
+    );
+    
+}
+
+export default WelcomePage;
--- guidr/src/App.js
+++ guidr/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import WelcomePage from '../components/WelcomePage';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello!!</h1>
+      <WelcomePage/>
     </div>
   );
 }
