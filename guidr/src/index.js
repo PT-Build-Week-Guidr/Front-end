20b18923859892838efd97e63631dd8fa829de63
ittyBittyBytes <loralie.flint0128@gmail.com>
2019-11-18 23:19:24 -0800

    index.js clean up

--- guidr/src/index.js
+++ guidr/src/index.js
@@ -1,25 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import App from "./App";
 import { Provider } from "react-redux";
 import reducer from "./API/reducer/index";
 import { BrowserRouter as Router } from "react-router-dom";
 import logger from "redux-logger";
 import 'bootstrap/dist/css/bootstrap.css'
-
 import './index.css';
-// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById("root")
-);
-
-
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
 
 const store = createStore(reducer, applyMiddleware(thunk, logger));
 
