ec86ea9cd6143a3dc9ecfffed35b0e4aa6155275
littleonetwo <littleonetwo@gmail.com>
2019-12-02 19:58:45 -0600

    fixed some errors

--- guidr/src/API/profile.js
+++ guidr/src/API/profile.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Api from "./AxiosHeader";
+import Api from "./axiosHeader";
 
 const Profile = props => {
   const [newTrip, setNewTrip] = useState({
